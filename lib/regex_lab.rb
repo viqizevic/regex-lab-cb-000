20be7ffe4cd8c83d82df0c0a9bd0911982152e7f
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-22 09:48:18 +0200

    Use scan method

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,15 +3,15 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.split.map { |e| e if /^un.*ing$/i.match?(e) }.compact
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.split.map { |e| e if /^[a-z]{5}$/i.match?(e) }.compact
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  /^[A-Z].*\.$/.match?(text)
+  /^[A-Z].*[\.!?]$/.match?(text)
 end
 
 def valid_phone_number?(phone)
